3d52d77b669e681cccf75f65319eeda89281493a
Adam Szabo <adam.street.fire@gmail.com>
2016-04-22 23:59:02 +0100

    Return Request instance of PostsFetcher method

--- LinkinBio/PostsFetcher.swift
+++ LinkinBio/PostsFetcher.swift
@@ -16,37 +16,40 @@ private let PostFetcherPostsKey : String = "posts"
 class PostFetcher : NSObject {
 	static let sharedFetcher : PostFetcher! = PostFetcher.init()
 	
-	func fetchPosts(completion : PostFetcherCompletion?) {
-		Alamofire.request(.GET, "https://app.latergram.me/api/pub/profiles.json?social_profile=latermedia", parameters: [:])
-			.responseJSON { response in
-				print(response.request)  // original URL request
-				print(response.result)     // server data
+	func fetchPosts(completion : PostFetcherCompletion?) -> Request? {
+		let request : Request = Alamofire.request(.GET, "https://app.latergram.me/api/pub/profiles.json?social_profile=latermedia", parameters: [:])
+		
+		request.responseJSON { response in
+			print(response.request)  // original URL request
+			print(response.result)     // server data
+			
+			let jsonResponse : [String : AnyObject]? = response.result.value as? [String : AnyObject]
+			
+			var posts : NSMutableArray? = nil
+			
+			if (jsonResponse != nil) {
+				print("Response: \(jsonResponse)")
 				
-				let jsonResponse : [String : AnyObject]? = response.result.value as? [String : AnyObject]
+				let postsMetadatas : NSArray? = jsonResponse![PostFetcherPostsKey] as? NSArray
 				
-				var posts : NSMutableArray? = nil
-				
-				if (jsonResponse != nil) {
-					print("Response: \(jsonResponse)")
+				if (postsMetadatas != nil) {
+					posts = NSMutableArray.init(capacity: postsMetadatas!.count)
 					
-					let postsMetadatas : NSArray? = jsonResponse![PostFetcherPostsKey] as? NSArray
-
-					if (postsMetadatas != nil) {
-						posts = NSMutableArray.init(capacity: postsMetadatas!.count)
+					postsMetadatas!.enumerateObjectsUsingBlock({ (postMetadata : AnyObject, index : Int, stop : UnsafeMutablePointer<ObjCBool>) in
+						let realPostMetadata = postMetadata as! [String : AnyObject]
 						
-						postsMetadatas!.enumerateObjectsUsingBlock({ (postMetadata : AnyObject, index : Int, stop : UnsafeMutablePointer<ObjCBool>) in
-							let realPostMetadata = postMetadata as! [String : AnyObject]
-							
-							let post = Post.init(metadata: realPostMetadata)
-							
-							posts?.addObject(post)
-						})
-					}
+						let post = Post.init(metadata: realPostMetadata)
+						
+						posts?.addObject(post)
+					})
 				}
-				
-				if (completion != nil) {
-					completion!(posts: posts?.copy() as? [Post]);
-				}
+			}
+			
+			if (completion != nil) {
+				completion!(posts: posts?.copy() as? [Post]);
+			}
 		}
+		
+		return request
 	}
 }
